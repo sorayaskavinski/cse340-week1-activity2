58f272f40e8c9f4b52c467957ad368c40bbac40a
sorayaskavinski <soraya1br@gmail.com>
2025-07-30 14:21:40 -0300

    week 4 assignments complet and working

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,13 +1,36 @@
 const express = require("express")
 const router =new express.Router()
 const accountController = require("../controllers/accountController")
-const utilities = require("../utilities/")  
+const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
 
+const { validationResult } = require("express-validator")
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * *****************************/
+async function checkRegData(req, res, next){
+    const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    return res.status(400).render("account/register", {
+      errors: errors.array(), // manda array de erros para a view
+      title: "Register",
+      nav,
+      account_firstname: req.body.account_firstname,
+      account_lastname: req.body.account_lastname,
+      account_email: req.body.account_email,
+    })
+  }
+  next()
+}
+
+/* ROUTES*/
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
-
-//Registration route
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
 
+/** ROUTER POST */
+router.post("/register", regValidate.registationRules(),
+checkRegData, utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router
